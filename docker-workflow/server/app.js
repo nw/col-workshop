a7d72430065346e83cb862d49ffb53ab51e0c8cf
Nathan White <nw@nwhite.net>
2018-12-07 08:43:54 -0600

    Update app.js

--- docker-workflow/server/app.js
+++ docker-workflow/server/app.js
@@ -13,6 +13,18 @@ if (!module.parent) {
     console.error(err.stack || err)
     process.exit(1)
   })
+  
+   process.addListener('unhandledRejection', function (err) {
+    console.error('Unhandled Rejection!')
+    console.error(err.stack || err)
+    process.exit(1)
+  })
+
+   process.addListener('multipleResolves', function (err) {
+    console.error('Multiple Resolves!')
+    console.error(err.stack || err)
+    process.exit(1)
+  })
 
   app.listen(app.get('port'), function () {
     console.error('\x1b[32mExample App\x1b[0m running on http://%s:%d',
